9412f591fdd998c7c93c8a3c40d46ca1725243b4
Alymbek996 <dragoikisa@gmail.com>
2022-03-18 16:29:08 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+	 Scanner S = new Scanner(System.in);
+        System.out.println("Введите ваше имя");
+        String string = S.nextLine( );
+        System.out.println(" Привет " + string);
+        System.out.println(" здравствуйте сенсей" );
+        }
+
+        }
+
+
